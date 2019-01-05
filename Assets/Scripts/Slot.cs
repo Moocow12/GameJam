9c0bdc5112e5e6935592307c2394108bcba537cb
Weston <westong123456@gmail.com>
2019-01-04 19:21:58 -0600

    Inventory Base Scripts added

---
+++ Assets/Scripts/Slot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot  {
+
+    public List<Item> items;
+
+    public void AddItem(Item item)
+    {
+        if(items.Count > 0)
+        {
+
+        }
+        else
+        {
+            items.Add(item);
+        }
+    }
+
+    public bool IsEmpty()
+    {
+        if(items.Count ==0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+
+}
